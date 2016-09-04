f5b25beefbbd6fd7646ef717c780ba2d7156887d
228806684@qq.com <228806684@qq.com>
2016-09-04 22:24:36 +0800

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,24 @@
 $(function ($) {
+
   $('#section_1').particleground({
-    height:'100%',
+    height: "auto",
     dotColor: '#999',
     lineColor: '#999'
   });
+
+  $('#wrapper').fullpage({
+    anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+    menu: '#menu',
+    css3: 'true',
+    scrollingSpeed: 500,
+    easingcss3: 'cubic-bezier(0.47, 0, 0.75, 0.72)',
+    afterLoad: function(anchorLink,index) {
+      if(index===2) {
+        // $(".service-item").css('margin-top','20px');
+        // console.log($(".service-item"))
+      }
+    }
+  });
+
+
 });
